7aa667e39f752521dffef90d2898f6e20d33c3f3
Pranjal Patra <pranjal.cna@gmail.com>
2021-03-05 17:09:39 -0330

    added some imports

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-print('Hello World. This is being written in PyCharm. Will be pushed and hopefully shows up on GitHub.')
-print('new line')
+import tkinter as tk
+from tkinter import ttk
+import mysql.connector
+
+
+
+print('test')
